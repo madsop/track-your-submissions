ed0f0bb4de2302448069d74221145ff7fb3997da
Mads Opheim <mads.opheim@gmail.com>
2019-10-15 20:58:09 +0200

    Added support for cospeakers

--- src/main/kotlin/org/acme/rest/talks/Talk.kt
+++ src/main/kotlin/org/acme/rest/talks/Talk.kt
@@ -3,6 +3,6 @@ package org.acme.rest.talks
 import java.util.*
 
 
-data class Talk(val id: UUID, val title: String) {
-    constructor(talkRequest: TalkRequest) : this(UUID.randomUUID(), talkRequest.title)
+data class Talk(val id: UUID, val title: String, val cospeaker: String?) {
+    constructor(talkRequest: TalkRequest) : this(UUID.randomUUID(), talkRequest.title, talkRequest.cospeaker)
 }
--- src/main/kotlin/org/acme/rest/talks/TalkRequest.kt
+++ src/main/kotlin/org/acme/rest/talks/TalkRequest.kt
@@ -1,8 +1,10 @@
 package org.acme.rest.talks
 
 data class TalkRequest(
-    var title: String
+    var title: String,
+    var cospeaker: String?
 )
 {
-    constructor(): this("")
+    constructor(): this( "")
+    constructor(title: String): this(title, "")
 }
