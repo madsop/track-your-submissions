4d80330556e6f4719126e6082de6d30b02884720
Mads Opheim <mads.opheim@gmail.com>
2020-06-27 13:32:52 +0200

    Fixed and simplified test

--- src/test/kotlin/org/acme/rest/submissions/SubmissionResourceHTTPTest.kt
+++ src/test/kotlin/org/acme/rest/submissions/SubmissionResourceHTTPTest.kt
@@ -3,6 +3,8 @@ package org.acme.rest.submissions
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
+import org.acme.rest.exposed.UUIDRequest
+import org.acme.rest.talks.TalkRequest
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
@@ -10,19 +12,18 @@ open class SubmissionResourceHTTPTest {
 
     @Test
     fun canPostSubmission() {
-        given().`when`().contentType(ContentType.JSON)
-                .body("{\n" +
-                        "\t\"title\": \"TalkTitle1\"\n" +
-                        "}")
+        val talkId: String = given().`when`().contentType(ContentType.JSON)
+                .body(TalkRequest(title = "TalkTitle1"))
                 .post("/talks/add")
-        given() //TODO: finn ut korleis chain-e httpkall i dette gwt-verktyget
+                .then()
+                .extract()
+                .path("id")
+        val request = SubmissionRequest(conference = "Confer", time = "2020", talk = UUIDRequest(talkId), notes = "emptyNotes")
+
+        given()
                 .`when`()
                 .contentType(ContentType.JSON)
-                .body("{\n" +
-                        "\t\"conference\": \"Confer\",\n" +
-                        "\t\"time\": 2020,\n" +
-                        "\t\"talk\": \"TalkTitle1\"\n" +
-                        "}")
+                .body(request)
                 .post("/cfp/add")
                 .then()
                 .statusCode(200)
